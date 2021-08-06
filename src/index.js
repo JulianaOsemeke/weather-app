d428d7ad9eb2e7276ad82a5939342d685da19381
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-08-07 00:04:22 +0100

    Create function to update weather  & Add Event listener to form

--- src/index.js
+++ src/index.js
@@ -18,3 +18,83 @@ const cityName = document.querySelector('.city-name p');
 const cardBody = document.querySelector('.card-body');
 const timeImage = document.querySelector('.card-top img');
 const roundCard = document.querySelector('.back-card');
+
+const convertToCelsius = (kelvin) => {
+  celsius = Math.round(kelvin - 273.15);
+  return celsius;
+  }
+  
+  isDayTime = (icon) => {
+  if (icon.includes('d')) {
+  return true;
+  } else {
+  return false;
+  }
+  }
+  
+  updateWeather = (city) => {
+  console.log(city);
+  const imageName = city.weather[0].icon;
+  const iconSrc = `http://openweathermap.org/img/wn/${imageName}@2x.png`
+  cityName.textContent = city.name;
+  cardBody.innerHTML = `
+  <div class="card-mid row">
+  <div class="col-8 text-center temp">
+  <span>${convertToCelsius(city.main.temp)}&deg;C</span>
+  </div>
+  <div class="col-4 condition-temp">
+  <p class="condition">${city.weather[0].description}</p>
+  <p class="high">${convertToCelsius(city.main.temp_max)}&deg;C</p>
+  <p class="low">${convertToCelsius(city.main.temp_min)}&deg;C</p>
+  </div>
+  </div>
+  <div class="icon-container card shadow mx-auto"> 
+  <img src="${iconSrc}" alt="">
+  </div>
+  <div class="card-bottom px-5 py-4 row">
+  <div class="col text-center">
+  <p>${convertToCelsius(city.main.feels_like)}&deg;C</p>
+  <span>feels like</span>
+  </div>
+  </div>
+  <div class="col text-center">
+  <p>${city.main.humidity}</p>
+  <span>Humidity</span>
+  </div>
+  `;
+  if (isDayTime(imageName)) {
+  console.log('day');
+  timeImage.setAttribute('src', 'Assets/sunny.svg');
+  if (cityName.classList.contains('text-white')) {
+  cityName.classList.remove('text-black');
+  
+  } else {
+  cityName.classList.add('text-black')
+  }
+  } else {
+  console.log('night')
+  timeImage.setAttribute('src', 'Assets/night-sky.svg');
+  if(cityName.classList.contains('text-black')){
+  cityName.classList.remove('text-black');
+  } else {
+  cityName.classList.add('text-white');
+  }
+  }
+  roundCard.classList.remove('d-none');
+  }
+  
+  //Add EventListener to form
+  searchForm.addEventListener('submit', (event)=> {
+  event.preventDefault();
+  const citySearched = cityValue.value.trim();
+  console.log(citySearched);
+  searchForm.reset();
+  
+  requestCity(citySearched)
+  .then((data) => {
+  updateWeather(data);
+  })
+  .catch((error) => {console.log(error)})
+  })
+  
+  
