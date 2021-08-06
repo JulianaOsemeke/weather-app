6f27805be787905d6a9edfce0088b337d8d813be
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-08-06 23:54:10 +0100

    Add promise call

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 const key = '30c88769ea6e3e295295f636c59bfc46';
+
+const requestCity = async (city) => {
+  const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+  const query = `?q=${city}&appid=${key}`;
+  
+  //make fetch call(promise call)
+  const response = await fetch(baseURL + query);
+  
+  //promise data
+  const data = await response.json();
+  return data;
+  }
