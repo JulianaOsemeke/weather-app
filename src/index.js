b800034198260801fb06343ddeb1bda5993d6beb
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-08-15 17:42:36 +0100

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import updateWeather from './dom';
 const key = '30c88769ea6e3e295295f636c59bfc46';
 
 const requestCity = async (city) => {
-  const baseURL = 'http://api.openweathermap.org/data/2.5/weather';
+  const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
   const query = `?q=${city}&appid=${key}`;
 
   const response = await fetch(baseURL + query);
